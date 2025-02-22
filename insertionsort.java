a7ba8177c154b3c8468b6be9e4b07cf1aa1ebb3b
YedruJahnavi <166897449+YedruJahnavi@users.noreply.github.com>
2025-02-22 10:47:12 +0530

    Create insertionsort.java

---
+++ insertionsort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class insertionsort {
+    public static void insertionsort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        // Read the elements
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        insertionsort(arr);
+
+        // Print the sorted array
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
