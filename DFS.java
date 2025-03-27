d5c408c4b3a1553165bf1bfe74f14e576d33a2a1
YedruJahnavi <166897449+YedruJahnavi@users.noreply.github.com>
2025-03-27 18:10:08 +0530

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -1,59 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class DFS 
-{
-    static void DFSRec(List<List<Integer> > adj,boolean[] visited, int s)
-    {
+class DFS {
+    // Recursive function for DFS traversal
+    static void dfsRec(List<List<Integer>> adj, boolean[] visited, int s) {
+        // Mark the current vertex as visited
         visited[s] = true;
+        // Print the current vertex
         System.out.print(s + " ");
-
+        // Recursively visit all adjacent vertices that are not visited yet
         for (int i : adj.get(s)) {
             if (!visited[i]) {
-                DFSRec(adj, visited, i);
+                dfsRec(adj, visited, i);
             }
         }
     }
 
-    static void DFS(List<List<Integer> > adj, int s) {
+    static void dfs(List<List<Integer>> adj, int s) {
         boolean[] visited = new boolean[adj.size()];
-
-        DFSRec(adj, visited, s);
+        dfsRec(adj, visited, s);
     }
 
-    static void addEdge(List<List<Integer> > adj,
-                               int s, int t){
-
-        adj.get(s).add(t);
-
-        adj.get(t).add(s);
+    static void addEdge(List<List<Integer>> adj, int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
     }
 
-
-    public static void main(String[] args)
-    {
-        int V = 5; 
-        List<List<Integer> > adj = new ArrayList<>(V);
+    public static void main(String[] args) {
+        int V = 5; // Number of vertices in the graph
+        // Create an adjacency list for the graph
+        List<List<Integer>> adj = new ArrayList<>(V);
         for (int i = 0; i < V; i++) {
             adj.add(new ArrayList<>());
         }
-
-        int[][] edges = {
-            { 1, 2 }, { 1, 0 }, { 2, 0 }, { 2, 3 }, { 2, 4 }
-        };
-
+        // Define the edges of the graph
+        int[][] edges = { { 1, 2 }, { 1, 0 }, { 2, 0 }, { 2, 3
+        }, { 2, 4 } };
         for (int[] e : edges) {
             addEdge(adj, e[0], e[1]);
         }
-
-        int source = 1; 
-        System.out.println("DFS from source: " + source);
-        DFS(adj, source); 
+        dfs(adj, 0);
     }
 }
-
-
-        OUTPUT
-DFS from source: 1
-            1 2 0 3 4 
-            
